03c68a64a6868633334c85782afae6ccdb7749e4
Kenny Molina <kjmolina@live.com>
2022-12-16 09:28:10 -0600

    add: new route, get params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,25 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'hi ken';
   }
+  @Get('nuevo')
+  newEndPoint() {
+    return 'soy nuevo';
+  }
+  // Two way of receiving params in routes
+  @Get('products/:id')
+  getPorducts(@Param() params: any) {
+    return `products id ${params.id}`;
+  }
+  @Get('products2/:id')
+  getPorducts2(@Param('id') id: any) {
+    return `products id ${id}`;
+  }
+  // obtain two parameters
+  @Get('products3/:id/:id2')
+  getPorducts3(@Param('id') id: any, @Param('id2') id2: any) {
+    return `products id ${id} y id2 ${id2}`;
+  }
+  
 }
