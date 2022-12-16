584e56cd8cba2c3a393f008db52bd6c831813ba6
Kenny Molina <kjmolina@live.com>
2022-12-16 14:34:44 -0600

    add: post method

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 /*
 Nota: @Controller ya nos crea la ruta por defecto de products, los metodos get solo deben escribirse la ruta extra aparte de products
 */
@@ -7,5 +7,9 @@ export class ProductsController {
   @Get('nuevo')
   newEndPoint() {
     return 'soy nuevo';
+  }
+  @Post()
+  set(@Body() params: any) {
+    return params;
   }
 }
