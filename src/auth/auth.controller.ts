cac4d1977cb7f690e96a2701cec347c755553b45
kenny molina <kenny.molina.murillo@gmail.com>
2023-07-11 17:10:50 -0600

    user roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,28 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { LogInDto } from './auth.dto';
-import { Role:rolesEnum } from "./../common/enums/roles.enum";
+import { Role } from './../common/enums/roles.enum';
+import { Roles } from './decorators/roles.decorator';
+import { RolesGuard } from './guards/roles.guard';
+import { ApiKeyGuard } from './guards/api-key.guard';
+import { Public } from './decorators/public.decorator';
 
+@UseGuards(ApiKeyGuard)
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: LogInDto) {
@@ -17,9 +31,11 @@ export class AuthController {
       userName: signInDto.username,
     });
   }
-  Roles(rolesEnum.Admin)
-  @Get()
-  roleEndPoint() {
+
+  @UseGuards(RolesGuard)
+  @Roles(Role.Admin)
+  @Post()
+  roleEndPoint(data: any): string {
     return 'Hola eres un admin';
   }
 }
